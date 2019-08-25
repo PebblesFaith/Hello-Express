2e3af3db34510ac2197bf4af4b44e290485a313e
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-08-25 16:26:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Create a reference to the express module.
 const express = require('express');
 
-// Use the express function to create an express web appliction object.
+// Use the express function to create an express web application object.
 const webapp = express();
 
 // Create a variable to store the port number, 3000 that my server will listen for a request.
 const port = 3000;
 
 // Use the express webapp.get function to create a route for the GET '/' endpoint.
-webapp.get('/hello/framework/exercise', function(request, response){
+webapp.get('/hello/framework/exercise', function(request, response){  
     response.send('Hello, World... I am learning how to write Node.js application codes using Express Framework API.');
 });
 
